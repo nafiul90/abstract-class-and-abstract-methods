588dc1bee603a941b560f1531a0cd785136d9128
nafiul90 <30845978+nafiul90@users.noreply.github.com>
2017-12-09 22:14:45 +0600

    Create Calculation.java

---
+++ src/abstractandinterface/Calculation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractandinterface;
+
+/**
+ *
+ * @author nafiul
+ */
+public interface Calculation {
+    
+    abstract double add(double n1,double n2);
+    abstract double substract(double n1,double n2);
+    abstract double multiply(double n1,double n2);
+    abstract double divition(double n1,double n2);
+    
+    
+        
+    
+    
+}
